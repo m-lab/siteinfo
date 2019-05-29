cf92607e158f37c53d04314ccf94053aca632d3a
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-05-29 08:34:09 -0400

    Update notes

--- create_siteinfo_api.sh
+++ create_siteinfo_api.sh
@@ -3,46 +3,46 @@
 # /configs
 # /v1/sites/locations.json
 # /v1/sites/hostnames.json
-# /v1/sites/measurement-lab.zone
-# /v1/sites/raw.json
-# /archive/
+# /v1/sites/sites.json
+# /v1/zones/measurement-lab.zone
 
-# +
-gcloud --project mlab-sandbox compute addresses create siteinfo-lb-ip-1 --ip-version=IPV4 --global
+PROJECT=${1:?Please provide project}
 
-# +
-gcloud --project mlab-sandbox compute backend-buckets create siteinfo-static-bucket \
-    --gcs-bucket-name siteinfo-mlab-sandbox
-
-# +
-gcloud --project mlab-sandbox compute url-maps create siteinfo-url-map \
-    --default-backend-bucket=siteinfo-static-bucket
-
-
-gcloud --project mlab-sandbox compute url-maps add-path-matcher siteinfo-url-map \
-    --path-matcher-name siteinfo-url-map-matcher \
-    --default-backend-bucket=siteinfo-static-bucket \
-    --backend-bucket-path-rules="/metadata/*=siteinfo-static-bucket" \
-    --new-hosts siteinfo.mlab-sandbox.measurementlab.net
+# TODO: make buckets.
+# siteinfo-${PROJECT} && empty-${PROJECT}
 
 # TODO: add ip lookup
+gcloud --project ${PROJECT} compute addresses create \
+  siteinfo-lb-ip-1 --ip-version=IPV4 --global
+
+# TODO: create backend bucket for empty-bucket.
+gcloud --project ${PROJECT} compute backend-buckets create \
+    siteinfo-backend-bucket \
+    --gcs-bucket-name siteinfo-${PROJECT}
+
+# TODO: create url-map with default to empty-bucket.
+gcloud --project ${PROJECT} compute url-maps create siteinfo-url-map \
+    --default-backend-bucket=siteinfo-backend-bucket
+
+# TODO: restrict all requests to /v1/* -- requires an "empty" default backend bucket.
+gcloud --project ${PROJECT} compute url-maps add-path-matcher siteinfo-url-map \
+    --path-matcher-name siteinfo-url-map-matcher \
+    --default-backend-bucket=siteinfo-backend-bucket \
+    --backend-bucket-path-rules="/v1/*=siteinfo-backend-bucket" \
+    --new-hosts siteinfo.${PROJECT}.measurementlab.net
+
 # TODO: add dns hostname update.
 
 # +
-gcloud --project mlab-sandbox beta compute ssl-certificates create siteinfo-ssl-cert \
-    --domains siteinfo.mlab-sandbox.measurementlab.net
+gcloud --project ${PROJECT} beta compute ssl-certificates create siteinfo-ssl-cert \
+    --domains siteinfo.${PROJECT}.measurementlab.net
 
 # +
-gcloud --project mlab-sandbox compute target-https-proxies create siteinfo-lb-proxy \
+gcloud --project ${PROJECT} compute target-https-proxies create siteinfo-lb-proxy \
     --url-map siteinfo-url-map --ssl-certificates siteinfo-ssl-cert
 
 # +
-gcloud --project mlab-sandbox compute forwarding-rules create siteinfo-fw-rule \
-    --address 35.190.26.149 --global \
+gcloud --project ${PROJECT} compute forwarding-rules create siteinfo-fw-rule \
+    --address ${LB_IP} --global \
     --target-https-proxy siteinfo-lb-proxy \
     --ports 443
-
-
-
-
-########
