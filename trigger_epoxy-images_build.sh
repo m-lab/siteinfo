eef12969d0f4a6099553cfdbd15c9da58e4777e4
Nathan Kinkade <kinkade@measurementlab.net>
2020-01-29 11:03:04 -0600

    Generates an auth bearer token for HTTPS request to Google API.

--- trigger_epoxy-images_build.sh
+++ trigger_epoxy-images_build.sh
@@ -5,11 +5,15 @@ set -x
 apt update > /dev/null
 apt install -y curl > /dev/null
 
+BASE_URL="https://cloudbuild.googleapis.com/v1/projects"
+
 TRIGGER_ID_mlab_sandbox="2a1bc430-6ae6-4d9d-9ae7-d75716a6077d"
 TRIGGER_ID_mlab_staging="98e3222b-e851-4622-86d1-e41368a72c47"
 TRIGGER_ID_mlab_oti="dbe996f9-eb52-4bd4-bde8-6712b3040002"
 
 TRIGGER_ID="TRIGGER_ID_${PROJECT_ID/-/_}"
+
+AUTH_TOKEN="$(gcloud config config-helper --format='value(credential.access_token)')"
 
 cat <<EOF > request.json
 {
@@ -19,6 +23,11 @@ cat <<EOF > request.json
 }
 EOF
 
-curl --request POST --data "@request.json" --header "Content-Type: application/json" \
-    "https://cloudbuild.googleapis.com/v1/projects/${PROJECT_ID}/triggers/${!TRIGGER_ID}:run"
+cat request.json
+
+curl --request POST \
+     --data "@request.json" \
+     --header "Content-Type: application/json" \
+     --header "Authorization: Bearer $AUTH_TOKEN" \
+     "${BASE_URL}/${PROJECT_ID}/triggers/${!TRIGGER_ID}:run"
 
