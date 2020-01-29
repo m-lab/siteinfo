ea54b566c8784cf8fffe223b59befd13bbe3a4eb
Nathan Kinkade <kinkade@measurementlab.net>
2020-01-29 13:49:52 -0600

    Uses an array of build trigger names for each project.

--- trigger_epoxy-images_build.sh
+++ trigger_epoxy-images_build.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
-
-set -x
+#
+# A simple script to run a Cloud Build trigger on a remote repo.
 
 BASE_URL="https://cloudbuild.googleapis.com/v1/projects"
 
-TRIGGER_ID_mlab_sandbox="2a1bc430-6ae6-4d9d-9ae7-d75716a6077d"
-TRIGGER_ID_mlab_staging="98e3222b-e851-4622-86d1-e41368a72c47"
-TRIGGER_ID_mlab_oti="dbe996f9-eb52-4bd4-bde8-6712b3040002"
+case $PROJECT_ID in
+mlab-sandbox)
+  TRIGGER_NAMES=(
+    epoxy-images-push-to-sandbox-stage1
+  )
+  ;;
+mlab-staging)
+  TRIGGER_NAMES=(
+    epoxy-images-push-to-master-stage1
+  )
+  ;;
+mlab-oti)
+  TRIGGER_NAMES=(
+    epoxy-images-tag-stage1
+  )
+  ;;
+*)
+  echo "Unknown project: ${PROJECT_ID}. Exiting..."
+  exit 1
+esac
 
-TRIGGER_ID="TRIGGER_ID_${PROJECT_ID/-/_}"
-
+# Generate an auth bearer token for the HTTPS request to the API.
 AUTH_TOKEN="$(gcloud config config-helper --format='value(credential.access_token)')"
 
+# Create the RepoSource request body document.
+# https://cloud.google.com/cloud-build/docs/api/reference/rest/Shared.Types/Build#RepoSource
 cat <<EOF > request.json
 {
   "projectId": "${PROJECT_ID}",
@@ -19,11 +37,20 @@ cat <<EOF > request.json
 }
 EOF
 
-cat request.json
-
-curl --request POST \
-     --data "@request.json" \
-     --header "Content-Type: application/json" \
-     --header "Authorization: Bearer $AUTH_TOKEN" \
-     "${BASE_URL}/${PROJECT_ID}/triggers/${!TRIGGER_ID}:run"
-
+for trigger in ${TRIGGER_NAMES[@]}; do
+  # Get the trigger ID for the named trigger.
+  trigger_id=$(
+    gcloud beta builds triggers list --filter "name=${trigger}" \
+        --format "value(id)" --project "${PROJECT_ID}"
+  )
+  if [[ -n "${trigger_id}" ]]; then
+    curl --request POST \
+        --data "@request.json" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: Bearer $AUTH_TOKEN" \
+        "${BASE_URL}/${PROJECT_ID}/triggers/${trigger_id}:run"
+  else
+    echo "Unable to determine trigger ID for named trigger: ${trigger}"
+    exit 1
+  fi
+done
