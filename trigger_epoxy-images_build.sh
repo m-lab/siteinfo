0444b4eb80054d1c31208c5625437e4a6191fa05
Nathan Kinkade <kinkade@measurementlab.net>
2020-01-29 10:51:10 -0600

    Use debian:stable-slim image and install curl in script.

--- trigger_epoxy-images_build.sh
+++ trigger_epoxy-images_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt update
+apt install -y curl
+
 TRIGGER_ID_mlab_sandbox="2a1bc430-6ae6-4d9d-9ae7-d75716a6077d"
 TRIGGER_ID_mlab_staging="98e3222b-e851-4622-86d1-e41368a72c47"
 TRIGGER_ID_mlab_oti="dbe996f9-eb52-4bd4-bde8-6712b3040002"
